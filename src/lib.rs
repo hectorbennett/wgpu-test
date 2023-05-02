ad258d7b10e0b17a6617508d88120b8995203a14
Hector <hectorbennett@outlook.com>
2023-05-02 11:51:34 +0100

    Add a canvas to the page

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use wasm_bindgen::prelude::*;
+use web_sys::HtmlCanvasElement;
 
 #[wasm_bindgen]
 extern "C" {
@@ -8,4 +9,27 @@ extern "C" {
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello world!");
+}
+
+fn init_canvas() -> Result<(), JsValue> {
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
+    let canvas: HtmlCanvasElement = document
+        .create_element("canvas")
+        .unwrap()
+        .dyn_into::<HtmlCanvasElement>()
+        .unwrap();
+
+    canvas.set_width(500);
+    canvas.set_height(500);
+    body.append_child(&canvas)?;
+
+    Ok(())
+}
+
+#[wasm_bindgen]
+pub fn render_canvas() -> Result<(), JsValue> {
+    init_canvas()?;
+    Ok(())
 }
